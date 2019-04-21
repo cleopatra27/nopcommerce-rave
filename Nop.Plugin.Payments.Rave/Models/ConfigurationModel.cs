a006cb00361e40bb6f16dde99aff56c865831da6
cleopatra <cleopatra@flutterwavego.com>
2019-04-21 14:18:34 +0100

    start project

---
+++ Nop.Plugin.Payments.Rave/Models/ConfigurationModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using Nop.Web.Framework.Models;
+namespace Nop.Plugin.Payments.Rave.Models
+{
+    public class ConfigurationModel
+    {
+        #region Ctor
+
+        public ConfigurationModel()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        [NopResourceDisplayName("Plugins.Payments.Rave.Fields.SecretKey")]
+        public string SecretKey { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Rave.Fields.PublicKey")]
+        public string Publickey { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Rave.Fields.EncryptKey")]
+        public string EncryptKey { get; set; }
+
+        #endregion
+    }
+}
